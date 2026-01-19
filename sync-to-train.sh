0deaf4547763b7f85341bfd0891c143d2f160eff
Sean Gallagher <stgallag@gmail.com>
2026-01-19 09:16:48 -0500

    Don't sync code anymore

--- sync-to-train.sh
+++ sync-to-train.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Sync code to the GPU machine
-rsync -av --dry-run ~/sandbox/inputctl/ sean-gallagher@intuition.local:/home/sean-gallagher/sandbox/inputctl/ --exclude dataset --exclude '*.venv' --exclude target --exclude node_modules --delete
-
 # Sync datasets and checkpoints both ways
 
 rsync -av ~/sandbox/inputctl/dataset/ sean-gallagher@intuition.local:/home/sean-gallagher/sandbox/inputctl/dataset/
